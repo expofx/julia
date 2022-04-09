72a08fc01e6eae512a1ab64819b9ce14cc35a7d7
Ellen Xu <xu.ellen@outlook.com>
2022-04-09 15:39:53 -0700

    first commit

---
+++ manipulations.jl
@@ -0,0 +1,32 @@
+### A Pluto.jl notebook ###
+# v0.19.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ dd652b00-b81d-11ec-1cce-e9fe25f68af9
+
+
+# ╔═╡ b82b72b4-7dd8-4d28-b126-b273f448e3ee
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═dd652b00-b81d-11ec-1cce-e9fe25f68af9
+# ╠═b82b72b4-7dd8-4d28-b126-b273f448e3ee
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
